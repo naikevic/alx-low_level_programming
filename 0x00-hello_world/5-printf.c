5238e9e30f5d2ea09fa40beb0cee4ccf1e51e993
naikevic <strongsam95@gmail.com>
2022-03-10 05:26:47 -0800

    commit for print c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /*
  * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
- * Retuen - 0 if exited properly, non-zero otherwise
- * */
+ * Return - 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
